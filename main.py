1b55b18c5e407f1ea9686f3d1178059dd549bd36
Ryan Shrott <ryanshrott@gmail.com>
2023-07-03 15:36:44 -0400

    variable

--- main.py
+++ main.py
@@ -11,11 +11,14 @@ import os
 from dotenv import load_dotenv
 from fastapi import FastAPI, HTTPException, Response, status
 from fastapi.responses import RedirectResponse
+from fastapi import FastAPI, HTTPException, Response, status
+from fastapi.responses import HTMLResponse
 load_dotenv(".env")
 
 app = FastAPI()
 airtable = Airtable(os.environ['AIRTABLE_BASE_ID'], os.environ['AIRTABLE_TABLE_NAME'], api_key=os.environ['AIRTABLE_API_KEY'])
-
+email_base_url = os.environ['EMAIL_BASE_URL']
+print(email_base_url)
 class EmailSchema(BaseModel):
     email: EmailStr
     id: Optional[str]
@@ -65,7 +68,7 @@ async def send_verification(email: EmailSchema):
         })
 
     # generate the email content
-    msg = f'<p>Welcome to SmartBids.ai!</p><p>Please click on the following link to verify your email:</p><a href="https://fastapi-production-86e9.up.railway.app/verify_email?token={token}&email={quote(email.email)}">Verify Email</a><p>Thank you,</p><p>SmartBids.ai Team</p>'
+    msg = f'<p>Welcome to SmartBids.ai!</p><p>Please click on the following link to verify your email:</p><a href="{email_base_url}/verify_email?token={token}&email={quote(email.email)}">Verify Email</a><p>Thank you,</p><p>SmartBids.ai Team</p>'
     subject = 'Email verification'
 
     # send the email
@@ -74,15 +77,29 @@ async def send_verification(email: EmailSchema):
     return {"message": "Verification email sent"}
 
 
-@app.get("/verify_email")
+
+@app.get("/verify_email", response_class=HTMLResponse)
 async def verify_email(token: str, email: str):
     # search for the record with the given email and token
     records = airtable.search('email', email)
     for record in records:
         if record['fields'].get('token') == token:
-            # update the verified field to True
-            airtable.update(record['id'], {'verified': True})
-            return RedirectResponse(url='https://app.smartbids.ai', status_code=status.HTTP_303_SEE_OTHER)
+            # check if the email is already verified
+            if record['fields'].get('verified', False):
+                return """
+                <h1>This email has already been verified!</h1>
+                <p>You are fully verified and can now login.</p>
+                <a href="https://app.smartbids.ai">Click here to login</a>
+                """
+            else:
+                # if not verified, update the verified field to True
+                airtable.update(record['id'], {'verified': True})
+                return """
+                <h1>Your email has been successfully verified!</h1>
+                <p>You are fully verified and can now login.</p>
+                <a href="https://app.smartbids.ai">Click here to login</a>
+                """
 
     raise HTTPException(status_code=400, detail="Invalid token or email")
+
 
