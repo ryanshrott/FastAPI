768617927821983b25e6dadd34260b9b39196ec4
Ryan Shrott <ryanshrott@gmail.com>
2023-08-29 18:59:35 -0400

    commit

--- main.py
+++ main.py
@@ -1,30 +1,44 @@
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI, HTTPException, Response, status
+from fastapi.responses import HTMLResponse, RedirectResponse
 from pydantic import BaseModel, EmailStr
 from typing import Optional
 from urllib.parse import quote
 import smtplib
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
-from airtable import Airtable
 import secrets
 import os
 from dotenv import load_dotenv
-from fastapi import FastAPI, HTTPException, Response, status
-from fastapi.responses import RedirectResponse
-from fastapi import FastAPI, HTTPException, Response, status
-from fastapi.responses import HTMLResponse
+from pymongo import MongoClient
+
 load_dotenv(".env")
 
 app = FastAPI()
-airtable = Airtable(os.environ['AIRTABLE_BASE_ID'], os.environ['AIRTABLE_TABLE_NAME'], api_key=os.environ['AIRTABLE_API_KEY'])
+
+# MongoDB setup
+mongo_uri = os.environ['MONGO_URI']
+client = MongoClient(mongo_uri)
+db = client['smartbids']
+users_collection = db['users']
+leads_collection = db['leads']
+
 email_base_url = os.environ['EMAIL_BASE_URL']
-print(email_base_url)
+
+
 class EmailSchema(BaseModel):
     email: EmailStr
     id: Optional[str]
 
+
+class LeadSchema(BaseModel):
+    name: str
+    email: EmailStr
+    phone: str
+    id: Optional[str]
+
+
 def send_email(subject, message, to_address):
-    print('trying to send email')
+    # Your send_email logic, unchanged from before
     from_address = 'ryan@smartbids.ai'
     password = os.getenv("EMAIL_PASS")
     msg = MIMEMultipart()
@@ -36,70 +50,101 @@ def send_email(subject, message, to_address):
     server.login(from_address, password)
     text = msg.as_string()
     server.sendmail(from_address, to_address, text)
-    print('email sent')
     server.quit()
 
-@app.post("/send_verification")
-async def send_verification(email: EmailSchema):
-    # generate a random token
+
+@app.post("/create_lead")
+async def create_lead(lead: LeadSchema):
     token = secrets.token_hex(20)
+    existing_lead = leads_collection.find_one({'email': lead.email})
 
-    # search for the record with the given email
-    records = airtable.search('email', email.email)
-
-    if records:
-        # if a record exists
-        record = records[0]
-        if record['fields'].get('verified', False):
-            # if the record is already verified, no need to send another email
+    if existing_lead:
+        if existing_lead.get('verified', False):
             return {"message": "Email is already verified"}
 
-        # update the existing record with a new token and set verified to False
-        airtable.update(record['id'], {
+        leads_collection.update_one(
+            {'_id': existing_lead['_id']},
+            {
+                "$set": {
+                    "name": lead.name,
+                    "phone": lead.phone,
+                    "token": token,
+                    "verified": False
+                }
+            }
+        )
+    else:
+        leads_collection.insert_one({
+            "name": lead.name,
+            "email": lead.email,
+            "phone": lead.phone,
             "token": token,
             "verified": False
         })
+
+    # [Rest of your email generation and sending logic]
+    msg = f'<p>Welcome to SmartBids.ai, {lead.name}!</p><p>Please click on the following link to verify your email:</p><a href="{email_base_url}/verify_client?token={token}&email={quote(lead.email)}&phone={quote(lead.phone)}&db_type=leads">Verify Email</a><p>Thank you,</p><p>SmartBids.ai Team</p>'
+    subject = 'Email verification'
+    send_email(subject, msg, lead.email)
+
+    return {"message": "Verification email sent"}
+
+
+@app.post("/send_verification")
+async def send_verification(email: EmailSchema):
+    token = secrets.token_hex(20)
+    existing_user = users_collection.find_one({'email': email.email})
+
+    if existing_user:
+        if existing_user.get('verified', False):
+            return {"message": "Email is already verified"}
+
+        users_collection.update_one(
+            {'_id': existing_user['_id']},
+            {
+                "$set": {
+                    "token": token,
+                    "verified": False
+                }
+            }
+        )
     else:
-        # if no record exists, create a new one
-        airtable.insert({
+        users_collection.insert_one({
             "email": email.email,
             "token": token,
             "verified": False
         })
 
-    # generate the email content
-    msg = f'<p>Welcome to SmartBids.ai!</p><p>Please click on the following link to verify your email:</p><a href="{email_base_url}/verify_email?token={token}&email={quote(email.email)}">Verify Email</a><p>Thank you,</p><p>SmartBids.ai Team</p>'
+    # [Rest of your email generation and sending logic]
+    msg = f'<p>Welcome to SmartBids.ai!</p><p>Please click on the following link to verify your email:</p><a href="{email_base_url}/verify_client?token={token}&email={quote(email.email)}&">Verify Email</a><p>Thank you,</p><p>SmartBids.ai Team</p>'
     subject = 'Email verification'
-
-    # send the email
     send_email(subject, msg, email.email)
 
     return {"message": "Verification email sent"}
 
 
+@app.get("/verify_client", response_class=HTMLResponse)
+async def verify_client(token: str, email: str, phone: str, db_type: str = "users"):
+    collection = users_collection if db_type == "users" else leads_collection
+    record = collection.find_one({'email': email, 'token': token})
 
-@app.get("/verify_email", response_class=HTMLResponse)
-async def verify_email(token: str, email: str):
-    # search for the record with the given email and token
-    records = airtable.search('email', email)
-    for record in records:
-        if record['fields'].get('token') == token:
-            # check if the email is already verified
-            if record['fields'].get('verified', False):
-                return """
-                <h1>This email has already been verified!</h1>
-                <p>You are fully verified and can now login.</p>
-                <a href="https://app.smartbids.ai">Click here to login</a>
-                """
-            else:
-                # if not verified, update the verified field to True
-                airtable.update(record['id'], {'verified': True})
-                return """
-                <h1>Your email has been successfully verified!</h1>
-                <p>You are fully verified and can now login.</p>
-                <a href="https://app.smartbids.ai">Click here to login</a>
-                """
+    if record:
+        if record.get('verified', False):
+            return """
+            <h1>This email has already been verified!</h1>
+            <p>You are fully verified and can now login.</p>
+            <a href="https://app.smartbids.ai">Click here to login</a>
+            """
+        else:
+            collection.update_one(
+                {'_id': record['_id']},
+                {"$set": {'verified': True}}
+            )
+            return """
+            <h1>Your email has been successfully verified!</h1>
+            <p>You are fully verified and can now login.</p>
+            <a href="https://app.smartbids.ai">Click here to login</a>
+            """
 
     raise HTTPException(status_code=400, detail="Invalid token or email")
-
 
